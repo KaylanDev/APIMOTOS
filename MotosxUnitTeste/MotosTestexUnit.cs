62a179953d1e859541f68de4097edabeddfcd03b
KaylanDev <k1ndealblack@gmail.com>
2025-04-27 21:28:02 -0300

    feat: add teste xunit get ate o momento, adcionar todos assim q possivel

---
+++ MotosxUnitTeste/MotosTestexUnit.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Motos.Controllers;
+using MotosxUnitTeste.xUnitTeste;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotosxUnitTeste
+{
+   public class MotosTestexUnit : IClassFixture<MotosxUnitController>
+    {
+        private readonly MotosMController controller;
+
+        public MotosTestexUnit(MotosxUnitController conntroler)
+        {
+            controller = new MotosMController(conntroler.uof);
+        }
+
+        [Fact]
+        public async Task Get_return_OkResult()
+        {
+            //Act
+            var data = await controller.Get();
+
+            //Assert
+            data.Result.Should().BeOfType<OkObjectResult>().Which.StatusCode.Should().Be(200);
+
+        }
+    }
+}
---
+++ MotosxUnitTeste/xUnitTeste/MotosxUnitController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Motos.Data;
+using Motos.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotosxUnitTeste.xUnitTeste
+{
+    public class MotosxUnitController
+    {
+        public IUnitOfWork uof;
+        public static DbContextOptions<AppDbContext> dbContextOptions { get; }
+
+        public static string conectionString = "Data Source=SHEISLINDA;Initial Catalog=MotosProject;Integrated Security=True;Trust Server Certificate=True";
+        static MotosxUnitController()
+        {
+            dbContextOptions = new DbContextOptionsBuilder<AppDbContext>().UseSqlServer(conectionString).Options;
+        }
+
+        public MotosxUnitController()
+        {
+
+            var context = new AppDbContext(dbContextOptions);
+            uof = new UnitOfWork(context);
+
+        }
+
+
+
+    }
+}
