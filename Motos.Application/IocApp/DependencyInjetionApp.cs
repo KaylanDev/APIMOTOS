2dae6d4fbd8d128d5dfe31de872b1662f986ffc0
KaylanDev <k1ndealblack@gmail.com>
2025-05-23 00:44:38 -0300

    perf: criei um iocapp para servicos de application ficarem organizados

---
+++ Motos.Application/IocApp/DependencyInjetionApp.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using Motos.Application.Interfaces;
+using Motos.Application.Service;
+using Motos.Application.Services;
+using Motos.Domain.Services;
+using Motos.Application.TokenService;
+using Motos.Application.Validator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motos.Application.IocApp;
+
+public static class DependencyInjetionApp
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        services.AddValidatorsFromAssemblyContaining<MotoValidator>();
+        services.AddValidatorsFromAssemblyContaining<MarcaValidator>();
+        services.AddScoped<IMotosMService, MotosMService>();
+        services.AddScoped<IMarcaService, MarcaService>();
+        services.AddScoped<ICacheService, CacheService>();
+        services.AddScoped<IJwtTokenService, JwtTokenService>();
+
+        return services;
+    }
+
+ 
+
+}
--- Motos.CrossCutting/IoC/DependencyInjection.cs
+++ Motos.CrossCutting/IoC/DependencyInjection.cs
@@ -1,11 +1,11 @@
 ﻿
 
 using FluentValidation;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Motos.API.Model.Validator;
 using Motos.Application.Interfaces;
 using Motos.Application.Service;
 using Motos.Application.Services;
@@ -14,6 +14,8 @@ using Motos.Domain.Interfaces;
 using Motos.Infrastruture.Context;
 using Motos.Infrastruture.Repository;
 using System.ComponentModel.Design;
+using System.Reflection;
+using Motos.Application.IocApp;
 
 
 
@@ -21,21 +23,38 @@ namespace Motos.CrossCutting.IoC
 {
    public  static class DependencyInjection
     {
+        
         public static IServiceCollection Addinfrastruture(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(configuration.GetConnectionString("sqlserver"),b => b.MigrationsAssembly("Motos.Infrastruture")));
 
             //services.AddScoped<IUnitOfWork, UnitOfWork>();
-            services.AddValidatorsFromAssemblyContaining<MotoValidator>();
-            services.AddValidatorsFromAssemblyContaining<MarcaValidator>();
             services.AddScoped<IMotosRepository, MotosMRepository>();
             services.AddScoped<IMarcaRepository, MarcaRepository>();
-            services.AddScoped<IMotosMService, MotosMService>();
-            services.AddScoped<IMarcaService, MarcaService>();
             services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-            services.AddScoped<ICacheService,CacheService>();
-        
+            services.AddIdentity<ApplicationUser, ApplicationRoles>(options =>
+            {
+                // Configure as opções de segurança do Identity aqui
+                // Estas são as configurações de senha. Ajuste-as conforme suas necessidades de segurança
+                options.Password.RequireDigit = false; // Exige um número
+                options.Password.RequiredLength = 6; // Comprimento mínimo
+
+                // Opções de lockout (bloqueio da conta após falhas de login)
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(1);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // Opções de usuário
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._"; // Caracteres permitidos no nome de usuário
+
+                // Opções de SignIn (login)
+                options.SignIn.RequireConfirmedAccount = false; // Exige que a conta seja confirmada para login
+
+            }).AddEntityFrameworkStores<AppDbContext>()// Conecta o Identity ao Entity Framework Core e ao seu DbContext
+            .AddDefaultTokenProviders(); // Adiciona os provedores de token padrão(para redefinição de senha, confirmação de e-mail, etc.)
+            services.AddApplication();
+
             return services;
         }
     }
